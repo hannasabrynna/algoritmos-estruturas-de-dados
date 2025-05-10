041443e9759ba253751ce81bed2846b04acecdd0
Rafaela Neves <rafaelaneves2712@hotmail.com>
2025-05-10 05:50:00 -0300

    chore: add tratamento de exececao

--- Projetos/Blog/db.php
+++ Projetos/Blog/db.php
@@ -1,4 +1,7 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=codewave;charset=utf8', 'root', 'root');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=codewave;charset=utf8', 'root', 'root');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Erro: ' . $e->getMessage();
+}
