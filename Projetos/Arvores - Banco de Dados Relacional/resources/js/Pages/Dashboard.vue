4fc22fba0486f6270ec65f5e71213a723fb69642
Rafaela Neves <rafaelaneves2712@hotmail.com>
2025-07-25 02:29:27 -0300

    feat: autenticacao

---
+++ Projetos/Arvores - Banco de Dados Relacional/resources/js/Pages/Dashboard.vue
@@ -0,0 +1,24 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="SylvaDB" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                SylvaDB
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
